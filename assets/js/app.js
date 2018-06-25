41506dc5440920551f2642efed7f01418c793393
Frederic Cywier <frederic@cywier.com>
2018-06-25 12:05:34 +0000

    Img with webpack Encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,6 @@
 var $ = require('jquery');
 require('bootstrap');
+
+// Require all images
+const imagesContext = require.context('../images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
+imagesContext.keys().forEach(imagesContext);
